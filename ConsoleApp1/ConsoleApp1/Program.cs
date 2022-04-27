b41c828079cc9adecbc8d8bb5cfc217e0e091621
awolferret <104518827+awolferret@users.noreply.github.com>
2022-04-27 21:30:39 +0300

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ведите любимое число");
+            int favoritNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите худшее число");
+            int worstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваше любимое число {favoritNumber},худшим вы считаете = {worstNumber}");
+            int bufferNumber;
+            bufferNumber = favoritNumber;
+            favoritNumber = worstNumber;
+            worstNumber = bufferNumber;
+            Console.WriteLine($"теперь ваше любимаое число {favoritNumber},худшим вы считаете {worstNumber}");
         }
     }
 }
